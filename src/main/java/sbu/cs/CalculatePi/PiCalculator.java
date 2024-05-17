ddf1e4b6963fb1c43ed953690af37bcf92778dad
HoseinA05 <asf.hosein@gmail.com>
2024-05-17 10:45:26 +0330

    Completed PiCalculator exercise

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -1,27 +1,70 @@
 package sbu.cs.CalculatePi;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class PiCalculator {
 
-    /**
-     * Calculate pi and represent it as a BigDecimal object with the given floating point number (digits after . )
-     * There are several algorithms designed for calculating pi, it's up to you to decide which one to implement.
-     Experiment with different algorithms to find accurate results.
+    public static class calculatePI implements Runnable {
+        static MathContext mc = new MathContext(1100); // Increased precision
+        int n;
+        static final BigDecimal C = new BigDecimal(2).multiply(new BigDecimal(2).sqrt(mc), mc).divide(new BigDecimal(9801), mc);
 
-     * You must design a multithreaded program to calculate pi. Creating a thread pool is recommended.
-     * Create as many classes and threads as you need.
-     * Your code must pass all of the test cases provided in the test folder.
+        public calculatePI(int n) {
+            this.n = n;
+        }
 
-     * @param floatingPoint the exact number of digits after the floating point
-     * @return pi in string format (the string representation of the BigDecimal object)
-     */
+        public BigDecimal factorial(int n) {
+            BigDecimal temp = BigDecimal.ONE;
+            for (int i = 1; i <= n; i++) {
+                temp = temp.multiply(BigDecimal.valueOf(i), mc);
+            }
+            return temp;
+        }
 
-    public String calculate(int floatingPoint)
-    {
-        // TODO
-        return null;
+        @Override
+        public void run() {
+            BigDecimal nominator = factorial(4 * n).multiply(new BigDecimal(26390 * n + 1103), mc);
+            BigDecimal denominator = factorial(n).pow(4, mc).multiply(new BigDecimal(396).pow(4 * n, mc), mc);
+            BigDecimal result = C.multiply(nominator, mc).divide(denominator, mc);
+            addToSum(result);
+        }
+    }
+
+    private static BigDecimal sum = BigDecimal.ZERO;
+
+    public static synchronized void addToSum(BigDecimal num) {
+        sum = sum.add(num);
+    }
+
+    public String calculate(int floatingPoint) {
+        ExecutorService threadpool = Executors.newFixedThreadPool(4);
+
+        sum = BigDecimal.ZERO;
+
+        int numberOfTerms = 200; // Increase this to improve precision
+        for (int i = 0; i <= numberOfTerms; i++) {
+            calculatePI task = new calculatePI(i);
+            threadpool.execute(task);
+        }
+
+        threadpool.shutdown();
+
+        try {
+            threadpool.awaitTermination(10000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return BigDecimal.ONE.divide(sum, new MathContext(1100)).setScale(floatingPoint, RoundingMode.DOWN).toString();
     }
 
     public static void main(String[] args) {
-        // Use the main function to test the code yourself
+        PiCalculator p = new PiCalculator();
+        System.out.println(p.calculate(7));
     }
 }
